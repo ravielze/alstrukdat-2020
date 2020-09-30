beab29dcafbbb271aa4603dabdd3d6a61d32c62b
ravielze <ravielz20@gmail.com>
2020-09-30 23:29:32 +0700

    Praktikum 6 (100/100)

---
+++ Praktikum-6/mesinkata.c
@@ -0,0 +1,43 @@
+#include "mesinkar.h"
+#include "mesinkata.h"
+#include <stdio.h>
+
+boolean EndKata;
+Kata CKata;
+
+void IgnoreBlank(){
+    while (CC == BLANK){
+        ADV();
+    }
+}
+
+void STARTKATA(){
+    START();
+    IgnoreBlank();
+    if (CC == MARK){
+        EndKata = true;
+    } else {
+        EndKata = false;
+        ADVKATA();
+    }
+}
+
+void ADVKATA(){
+    IgnoreBlank();
+    if (CC == MARK && !EndKata){
+        EndKata = true;
+    } else{
+        SalinKata();
+        IgnoreBlank();
+    }
+}
+
+void SalinKata(){
+    int i = 0;
+    while ((CC != MARK) && (CC != BLANK) && i != NMax) {
+        CKata.TabKata[i] = CC;
+        ADV();
+        i++;
+    }
+    CKata.Length = (CKata.Length < NMax) ? i : NMax;
+}
