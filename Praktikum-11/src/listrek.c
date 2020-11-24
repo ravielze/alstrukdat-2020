c4bf039d45601a251077c6c51c794d60d636ea47
ravielze <ravielz20@gmail.com>
2020-11-25 04:56:37 +0700

    Praktikum 11 (Fullscore)

--- Praktikum-11/src/listrek.c
+++ Praktikum-11/src/listrek.c
@@ -119,3 +119,94 @@ boolean Search (List L, infotype X){
         }
     }
 }
+infotype MaxList (List L){
+    if (IsOneElmt(L) == 1){
+        return Info(L);
+    } else {
+        return (Info(L) > MaxList(Tail(L)) ? Info(L) : MaxList(Tail(L)));
+    }
+}
+
+infotype MinList (List L){
+    if (IsOneElmt(L) == 1){
+        return Info(L);
+    } else {
+        return (Info(L) < MinList(Tail(L)) ? Info(L) : MinList(Tail(L)));
+    }
+}
+
+infotype SumList (List L){
+    if (IsEmpty(L) == 1){
+        return 0;
+    } else {
+        return Info(L) + SumList(Tail(L));
+    }
+}
+
+float AverageList (List L){
+    return (SumList(L)*1.0f)/(NbElmtList(L)*1.0f);
+}
+
+List InverseList (List L){
+    if (IsEmpty(L)) {
+        return Nil;
+    } else {
+        return KonsB(InverseList(Tail(L)), Info(L));
+    }
+}
+
+void SplitPosNeg (List L, List *L1, List *L2){
+    if (IsEmpty(L)) {
+        *L1 = Nil;
+        *L2 = Nil;
+    } else {
+        SplitPosNeg(Tail(L), L1, L2);
+        if (Info(L) < 0) {
+            *L2 = Konso(Info(L), *L2);
+        } else {
+            *L1 = Konso(Info(L), *L1);
+        }
+    }
+}
+
+void SplitOnX (List L, infotype X, List *L1, List *L2){
+    if (IsEmpty(L)){
+        *L1 = Nil;
+        *L2 = Nil;
+    } else {
+        SplitOnX(Tail(L), X, L1, L2);
+        if(Info(L) >= X){
+            *L2 = Konso(Info(L), *L2);
+        }else{
+            *L1 = Konso(Info(L), *L1);
+        }
+    }
+}
+
+int ListCompare (List L1, List L2){
+    if (IsEmpty(L1) && IsEmpty(L2)){
+        return 0;
+    } else if (IsEmpty(L1) && (!IsEmpty(L2))){
+        return -1;
+    } else if ((!IsEmpty(L1) && IsEmpty(L2))){
+        return 1;
+    } else {
+        if (Info(L1) < Info(L2)) {
+            return -1;
+        } else if (Info(L1) > Info(L2)){
+            return 1;
+        } else {
+            return ListCompare(Tail(L1), Tail(L2));
+        }
+    }
+}
+
+boolean IsAllExist (List L1, List L2){
+    if (IsEmpty(L1) == 1){
+        return false;
+    } else if (IsOneElmt(L1) == 1){
+        return Search(L2, Info(L1));
+    } else {
+        return Search(L2, Info(L1)) && IsAllExist(Tail(L1), L2);
+    }
+}
